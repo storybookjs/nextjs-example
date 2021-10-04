5d9c3e3692219ea54faf0ddc05cdab1ec30afe13
Michael Chan <mijoch@gmail.com>
2021-10-04 16:59:19 -0700

    add first Next.js page to Storybook

---
+++ stories/pages/index.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Index from "../../pages/index";
+
+export default {
+  title: "Pages/Index",
+  component: Index,
+};
+
+export function IndexPage() {
+  return <Index />;
+}
