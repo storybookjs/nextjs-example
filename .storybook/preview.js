797be9675db2fe9e907b67ba94e83f937e35603b
Michael Chan <mijoch@gmail.com>
2021-10-05 10:50:38 -0700

    setup and configure msw

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,18 @@
 import "../styles/globals.css";
-
 import * as NextImage from "next/image";
+import { setupWorker, rest } from "msw";
+
+// https://mswjs.io/docs/getting-started/integrate/browser#start-worker
+if (typeof global.process === "undefined") {
+  const worker = setupWorker();
+  worker.start(
+    rest.get("https://localhost:3000/api/hello", (req, res, ctx) => {
+      return res(ctx.json({ name: "John Dough" }));
+    })
+  );
+
+  // assign for params
+}
 
 const OriginalNextImage = NextImage.default;
 
