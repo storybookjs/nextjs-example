8022bcb9c474cd2926262d448a6f3f5501b233ec
Michael Chan <mijoch@gmail.com>
2021-10-21 16:34:25 -0700

    Integrate MSW and Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,17 @@ import "../styles/globals.css";
 
 import * as NextImage from "next/image";
 
+import { setupWorker, rest } from "msw";
+
+if (typeof global.process === "undefined") {
+  const worker = setupWorker(
+    rest.get("http://localhost:3000/api/hello", (req, res, ctx) => {
+      return res(ctx.json({ name: "John Doh" }));
+    })
+  );
+  worker.start();
+}
+
 const OriginalNextImage = NextImage.default;
 
 Object.defineProperty(NextImage, "default", {
