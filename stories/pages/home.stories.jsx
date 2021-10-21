18d9ec99ef9878fc41b999cdc265d7e6ab2dfcf6
Michael Chan <mijoch@gmail.com>
2021-10-21 15:46:57 -0700

    Add Controls to Next.js Page Props with Story Args

--- stories/pages/home.stories.jsx
+++ stories/pages/home.stories.jsx
@@ -4,6 +4,12 @@ import Home from "../../pages/index";
 export default {
   title: "Pages/Home",
   component: Home,
+  // default args
+  args: {
+    name: "John Doe",
+  },
 };
 
-export const HomePage = () => <Home />;
+export const HomePage = (args) => <Home {...args} />;
+// story-specific args
+HomePage.args = { name: "John Dough" };
