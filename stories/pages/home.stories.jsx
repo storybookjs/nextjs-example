382c5861ef37cb74c24c5c5ae7b99ef675328e3e
Michael Chan <mijoch@gmail.com>
2022-02-12 21:54:22 -0800

    some stuff?

--- stories/pages/home.stories.jsx
+++ stories/pages/home.stories.jsx
@@ -4,30 +4,31 @@ import { rest } from "msw";
 
 export default {
   title: "Pages/Home",
+  // component: () => <div>hi</div>,
   component: Home,
 };
 
 export const WithoutName = {};
 
-export const WithName = { args: { name: "John Doe" } };
+// export const WithName = { args: { name: "John Doe" } };
 
-export const WithNameFromGSSP = {
-  render: (args, { loaded: props }) => <Home {...args} {...props} />,
-};
-WithNameFromGSSP.loaders = [
-  ({ parameters }) => {
-    const worker = parameters.msw;
-    worker.resetHandlers();
+// export const WithNameFromGSSP = {
+//   render: (args, { loaded: props }) => <Home {...args} {...props} />,
+// };
+// WithNameFromGSSP.loaders = [
+//   ({ parameters }) => {
+//     const worker = parameters.msw;
+//     worker.resetHandlers();
 
-    worker.use(
-      rest.get("http://localhost:3000/api/hello", (req, res, ctx) => {
-        return res(ctx.json({ name: "John Dough" }));
-      })
-    );
-  },
-  async () => {
-    let data = await getServerSideProps();
+//     worker.use(
+//       rest.get("http://localhost:3000/api/hello", (req, res, ctx) => {
+//         return res(ctx.json({ name: "John Dough" }));
+//       })
+//     );
+//   },
+//   async () => {
+//     let data = await getServerSideProps();
 
-    return data.props;
-  },
-];
+//     return data.props;
+//   },
+// ];
